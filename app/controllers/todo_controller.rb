76ef1a6c2e3a2cb6a0023babe04fd2960adb2e47
melissa160 <melissa160@gmail.com>
2017-04-18 11:18:12 -0500

    Adicionando metodo URI para que viajen por la url caracteres en utf-8

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,12 @@
 require "make_todo"
+require 'uri'
 
+#index
 get '/' do
   erb :index
 end
 
+#send json with todos
 get '/todos'  do
   todos = Tarea.all
   todos = todos.sort_by{ |t| t["id"] }
@@ -11,32 +14,23 @@ get '/todos'  do
   {todos: todos}.to_json
 end
 
+#create
 post '/todos' do
-  Tarea.create(params[:title])
-  # if new_todo.save 
+  val = URI.escape(params[:title])
+  Tarea.create(val) 
   redirect '/todos'
-  # end
 end
 
+#complete todo
 patch '/todos/:id' do
   todo = Tarea.find(params[:id])
-  puts "incompleta #{params[:incompleta]}"
-  if params[:incompleta] == "incompleta"
-    puts "*"*60
-    puts "tarea completa"
-  else
-    puts "*"*60
-    puts "no entra  a incompleta"
-  end
   todo["done"] = true
   Tarea.update(todo["id"])
   redirect '/todos'
 end
 
+#delete todo
 delete '/todos/:id' do
   Tarea.destroy(params[:id])
   redirect '/todos'
-end
-
-get '/todos/:id' do
 end
--- public/js/application.js
+++ public/js/application.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
   bindEvents();
 });
 
+//variables para usar Handlebars.js
 var source   = $("#todo-template").html();
 var template = Handlebars.compile(source);
+
+//objetos para meter en un arreglo los todos completados y los pendientes 
 var todosCompleted = {todos: []}
 var todosPending = {todos: []}
 
+//planto los eventos de escucha
 function bindEvents(){
   $(".crearTodo").off("submit");
   $(".delete").off("click");
@@ -18,11 +22,58 @@ function bindEvents(){
   $(".pending").on("click", completeTodo);
 }
 
+//obtengo la lista de todos
 function getTodos(){
   $.ajax({
     method: "GET",
     url: "/todos"
   }).done(render).fail()
+}
+
+//Crea un todo
+function createTodo(event){
+  event.preventDefault()
+  var url = this.action
+  var data = $(this).serialize()
+  $("input[type='text']").val("")
+  $.ajax({
+    url: url,
+    method: "POST",
+    data: encodeURI(data)
+  }).done(render).fail()
+}
+
+//Elimina un todo
+function deleteTodo(event){
+  event.preventDefault()
+  var url = this.href
+  $.ajax({
+    url: url,
+    method: "DELETE",
+  }).done(render).fail()
+}
+
+//Completa un todo
+function completeTodo(event){
+  event.preventDefault()
+  var url = this.href
+  $.ajax({
+    url: url,
+    method: "PATCH"
+  }).done(render).fail()
+}
+
+//muestra la lista de todos
+function render(response){
+  checkTodos(response)
+  console.log(todosPending)
+  console.log(todosCompleted)
+  var pending = template(todosPending)
+  // var pending = template(response)
+  $("#todo-pendientes").html(pending)
+  var completed = template(todosCompleted)
+  $("#todo-completados").html(completed)
+  bindEvents()
 }
 
 //verifica cuales todos se encuentran completados y cuales no
@@ -35,46 +86,4 @@ function checkTodos(response){
     }else
       todosPending.todos.push(todo)
   });  
-}
-
-function createTodo(event){
-  event.preventDefault()
-  var url = this.action
-  var data = $(this).serialize()
-  $("input[type='text']").val("")
-  $.ajax({
-    url: url,
-    method: "POST",
-    data: data
-  }).done(render).fail()
-}
-
-function deleteTodo(event){
-  event.preventDefault()
-  var url = this.href
-  $.ajax({
-    url: url,
-    method: "DELETE",
-  }).done(render).fail()
-}
-
-function completeTodo(event){
-  event.preventDefault()
-  var url = this.href
-  $.ajax({
-    url: url,
-    method: "PATCH"
-  }).done(render).fail()
-}
-
-function render(response){
-  checkTodos(response)
-  console.log(todosPending)
-  console.log(todosCompleted)
-  var pending = template(todosPending)
-  // var pending = template(response)
-  $("#todo-pendientes").html(pending)
-  var completed = template(todosCompleted)
-  $("#todo-completados").html(completed)
-  bindEvents()
 }
